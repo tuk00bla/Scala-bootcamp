dba1aff719790cd76202a852665cd604dce896d5
Egor Skuratovich <tukforyoutube@mail.ru>
2020-12-21 22:15:43 +0300

    Init commit

--- Program.cs
+++ Program.cs
@@ -139,10 +139,12 @@ namespace Task1
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a string:");
-            string enter = Console.ReadLine();
-            CheckEnter(enter);
-            Console.WriteLine(enter);
-            Console.ReadKey();
+            while (true)
+            {
+                string enter = Console.ReadLine();
+                CheckEnter(enter);
+                Console.WriteLine(enter);
+            }
         }
 
         private static string CheckEnter(string checkedString)
@@ -212,10 +214,16 @@ namespace Task1
                 Console.WriteLine(String.Join("; ", variant));
                 Console.WriteLine(comb);
             }
+<<<<<<< Updated upstream
 
             Console.WriteLine("Combination " + combinations.Max().ToString());
 
             return combinations.Max();
+=======
+            combinations.Sort(new CombinationComparer());
+            Console.WriteLine("Combination: " + combinations.Last() );
+            return combinations.Last();
+>>>>>>> Stashed changes
         }
 
         private static Combination FindCombination(IList<Card> cards, Dictionary<Rank, int> ranks, Dictionary<Suit, int> suits)
@@ -366,9 +374,244 @@ namespace Task1
             return false;
         }
 
+<<<<<<< Updated upstream
         static bool IsThreeOfAKind(IList<Card> combCards, Dictionary<Rank, int> ranks)
         {
             if (ranks.ContainsValue(3))
+=======
+        static Pair MakePair(IList<Card> combCards, Dictionary<Rank, int> ranks)
+        {
+            Rank highCard = Rank.Value2; // Use unassigment variable
+            Rank kicker = Rank.Value2;
+            List<Rank> potentialKickers = ranks.Keys.ToList();
+            foreach (KeyValuePair<Rank, int> entry in ranks)
+            {
+                if (entry.Value == 2)
+                {   highCard = entry.Key;}
+            }
+            potentialKickers.Remove(highCard);
+            potentialKickers.Sort();
+            kicker = potentialKickers.Max();
+            return new Pair(highCard, kicker);
+
+        }
+
+        static HighCard MakeHighCard(IList<Card> combCards, Dictionary<Rank, int> ranks)
+        {
+            Rank highCard = ranks.Keys.Max();
+            return new HighCard(highCard);
+
+        }
+       
+        public interface ICombination : IComparable
+        {
+            public Combination marker { get; }
+
+        }
+
+        public class StraightFlush : ICombination, IComparable
+        {
+            public Rank HighCard { get; }
+            public Combination marker { get; }
+            public StraightFlush(Rank r)
+            {
+                this.marker = Combination.StraightFLush;
+                this.HighCard = r;
+            }
+
+            public int CompareTo(object obj)
+            {
+                StraightFlush compObj = (StraightFlush)obj;
+
+                if (this.HighCard > compObj.HighCard)
+                    return 1;
+
+                if (this.HighCard < compObj.HighCard)
+                    return -1;
+
+                else
+                    return 0;
+            }
+            public override string ToString()
+            {
+                return "STRAIGHT FLUSH: RANK -> " + this.HighCard;
+            }
+        }
+
+        public class  FullHouse: ICombination, IComparable
+        {
+            public Rank TwoCard { get; }
+            public Rank ThreeCard { get; }
+            public Combination marker { get; }
+
+            public FullHouse(Rank th, Rank tw)
+            {
+                this.ThreeCard = th;
+                this.TwoCard = tw;
+                this.marker = Combination.FullHouse;
+            }
+
+            public int CompareTo(object obj)
+            {
+                FullHouse compObj = (FullHouse)obj;
+                if (this.ThreeCard > compObj.ThreeCard && this.TwoCard > compObj.TwoCard)
+                    return 1;
+
+                if (this.ThreeCard < compObj.ThreeCard && this.TwoCard < compObj.TwoCard)
+                    return -1;
+
+                else
+                    return 0;
+            }
+
+            public override string ToString()
+            {
+                return "FULL HOUSE: THREE CARD -> " + this.ThreeCard +  "TWO CARD ->" + this.TwoCard;
+            }
+
+        }
+
+        public class Straight : ICombination, IComparable
+        {
+           public Rank Rank { get; }
+            public Combination marker { get; }
+
+            public Straight(Rank r)
+            {
+                this.Rank = r;
+                this.marker = Combination.Straight;
+            }
+
+            public int CompareTo(object obj)
+            {
+                Straight compObj = (Straight)obj;
+
+                if (this.Rank > compObj.Rank)
+                    return 1;
+
+                if (this.Rank < compObj.Rank)
+                    return -1;
+
+                else
+                    return 0;
+            }
+
+            public override string ToString()
+            {
+                return "STRAIGHT: RANK -> " + this.Rank;
+            }
+        }
+
+
+
+        public class Flush : ICombination, IComparable
+        {
+            public Rank Rank { get; }
+            public Combination marker { get; }
+
+            public Flush(Rank r)
+            {
+                this.Rank = r;
+                this.marker = Combination.Flush;
+            }
+
+            public int CompareTo(object obj)
+            {
+                Flush compObj = (Flush)obj;
+
+                if (this.Rank > compObj.Rank)
+                    return 1;
+
+                if (this.Rank < compObj.Rank)
+                    return -1;
+
+                else
+                    return 0;
+            }
+
+            public override string ToString()
+            {
+                return "FLUSH: RANK -> " + this.Rank;
+            }
+        }
+
+        public class FourOfAKind : ICombination, IComparable
+        {
+            public Rank HighCard { get; }
+            public Rank Kicker { get; }
+            public Combination marker { get; }
+
+            public FourOfAKind(Rank r, Rank k)
+            {
+                this.HighCard = r;
+                this.Kicker = k;
+                this.marker = Combination.FourOfAKind;
+            }
+
+            public int CompareTo(object obj)
+            {
+                FourOfAKind compObj = (FourOfAKind)obj;
+                if (this.HighCard > compObj.HighCard)
+                    return 1;
+
+                if (this.HighCard < compObj.HighCard)
+                    return -1;
+
+                else
+                    return 0;
+            }
+
+            public override string ToString()
+            {
+                return "FOUR OF A KIND: RANK -> " + this.HighCard + " KICKER -> " + this.Kicker;
+            }
+        }
+
+        public class ThreeOfAKind : ICombination, IComparable
+        {
+            public Rank HighCard { get; }
+            public Rank Kicker { get; }
+            public Combination marker { get; }
+
+            public ThreeOfAKind(Rank r, Rank k)
+            {
+                this.HighCard = r;
+                this.Kicker = k;
+                this.marker = Combination.ThreeOfAKind;
+            }
+
+            public int CompareTo(object obj)
+            {
+                return 0;
+            }
+
+            public override string ToString()
+            {
+                return "THREE OF A KIND: RANK -> " + this.HighCard + " KICKER -> " + this.Kicker;
+            }
+        }
+
+        public class TwoPairs : ICombination, IComparable
+        {
+            public Rank HighPair { get; }
+            public Rank LowPair { get; }
+            public Rank Kicker { get; }
+            public Combination marker { get; }
+
+            public TwoPairs(Rank hp, Rank lp, Rank k)
+            {
+                this.HighPair = hp;
+                this.LowPair = lp;
+                this.Kicker = k;
+                this.marker = Combination.TwoPair;
+            }
+
+            public int CompareTo(object obj)
+            {
+                return 0;
+            }
+            public override string ToString()
+>>>>>>> Stashed changes
             {
               //  Console.WriteLine($"{{{combCards[0].Rank} {combCards[1].Rank} {combCards[2].Rank} {combCards[3].Rank} {combCards[4].Rank}}}");
                 return true;
@@ -378,17 +621,67 @@ namespace Task1
 
         static bool IsTwoPairs(IList<Card> combCards, Dictionary<Rank, int> ranks)
         {
+<<<<<<< Updated upstream
             if (ranks.Count == 3 && ranks.ContainsValue(2))
             {
               //  Console.WriteLine($"{{{combCards[0].Rank} {combCards[1].Rank} {combCards[2].Rank} {combCards[3].Rank} {combCards[4].Rank}}}");
                 return true;
+=======
+            public Rank pairRank { get; }
+            public Combination marker { get; }
+            public Rank Kicker { get; }
+            public Pair(Rank pr, Rank k)
+            {
+                this.pairRank = pr;
+                this.Kicker = k;
+                this.marker = Combination.Pair;
+            }
+            public int CompareTo(object obj)
+            {
+                Pair other = (Pair)obj; 
+                if (this.pairRank < other.pairRank) return -1;
+                if (this.pairRank > other.pairRank) return 1;
+                if (this.Kicker < other.Kicker) return -1;
+                if (this.Kicker > other.Kicker) return 1;
+                return 0;
+            }
+
+            public override string ToString()
+            {
+                return "PAIR: PAIR RANK -> " + this.pairRank + " KICKER -> " + this.Kicker;
+>>>>>>> Stashed changes
             }
             return false;
         }
 
         static bool IsPair(IList<Card> combCards, Dictionary<Rank, int> ranks)
         {
+<<<<<<< Updated upstream
             if (ranks.ContainsValue(2))
+=======
+            public Rank Kicker { get; }
+            public Combination marker { get; }
+            public HighCard(Rank r)
+            {
+                this.Kicker = r;
+                this.marker = Combination.HighCard;
+            }
+
+            public int CompareTo(object obj)
+            {
+                HighCard compObj = (HighCard)obj;
+
+                if (this.Kicker > compObj.Kicker)
+                    return 1;
+
+                if (this.Kicker < compObj.Kicker)
+                    return -1;
+
+                else
+                    return 0;
+            }
+            public override string ToString()
+>>>>>>> Stashed changes
             {
                // Console.WriteLine($"{{{combCards[0].Rank} {combCards[1].Rank} {combCards[2].Rank} {combCards[3].Rank} {combCards[4].Rank}}}");
                 return true;
@@ -396,6 +689,7 @@ namespace Task1
             return false;
         }
 
+<<<<<<< Updated upstream
       /*  public static char RankToString(Rank r)
         {
             switch (r)
@@ -410,6 +704,18 @@ namespace Task1
                     return 's';
                 default:
                     return 'g';
+=======
+        public class CombinationComparer : IComparer<ICombination>
+        {
+            public int Compare(ICombination x, ICombination y)
+            {
+                if (x.marker > y.marker)
+                    return 1;
+
+                if (x.marker < y.marker)
+                    return -1;
+                return x.CompareTo(y);
+>>>>>>> Stashed changes
             }
         } */
 
